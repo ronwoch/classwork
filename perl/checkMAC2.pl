dc1c7be2852c56c112b2cd66da584610450e21d7
ninja <ninja@localhost.localdomain>
2017-05-22 11:29:38 -0700

    fixed perl/checkMAC2.pl

--- perl/checkMAC2.pl
+++ perl/checkMAC2.pl
@@ -8,18 +8,18 @@
 
 # fetch the OUI database from IEEE
 
-`wget -N http://standards.ieee.org/develop/regauth/oui/oui.txt 2>/dev/null`;
+# `wget -N http://standards.ieee.org/develop/regauth/oui/oui.txt 2>/dev/null`;
 
 open (LOG, ">>maclog.log") || die "Cannot open maclog.log for append $!\n";
 
 $netinfo =`ifconfig | grep ether`;
-print "network information is ",$netinfo,"\n";
-print LOG "network information is ",$netinfo,"\n";
+print "\nnetwork information is \n",$netinfo,"\n";
+print LOG "\nnetwork information is ",$netinfo,"\n";
 $netinfo =~ m/(([0-9a-f]{2}[:-]){5}[0-9a-f]{2})/i;
 
 $mac = $&; # $& is the previous successful match
-print "the MAC address is ",$mac,"\n";
-print LOG "the MAC address is ",$mac,"\n";
+print "\nthe MAC address is ",$mac,"\n";
+print LOG "\nthe MAC address is ",$mac,"\n";
 @macparts = split /:/, $mac;
 
 @ouiparts = splice(@macparts,0,3);
@@ -31,7 +31,7 @@ print LOG "the OUI is ",$oui,"\n";
 open (OUIDB,"oui.txt") || die "Cannot open oui.txt $!\n";
 while (<OUIDB>){
     $line = $_; # $_ is the implicit scalar variable
-    print "line is ", $line,"oui is ",$oui,"\n";
+    # print "line is ", $line,"oui is ",$oui,"\n";
     if ($line =~ /$oui/i) {
         @ouientry = $line;
         last;
